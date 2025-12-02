af5bf19add1f87da9492b35b8fdc26c771794a86
Aylén Frísoli <aylen@trece.ar>
2025-12-02 12:22:38 -0300

    Clase 9

---
+++ clase-9/nombres-vinculantes.js
@@ -0,0 +1,26 @@
+//ejemplos de nombres de vinculacione svalidos
+
+let nombre = "Aylén";
+let _nombre = "Aylu";
+
+let $preciofinal = 2711;
+let firstname = "Aylén";
+
+//ejemplo de intento de uso de una palbra clave
+
+let if = true; //esto no es valido
+
+//ejemplo de intento de uso de una palabra clave
+
+let class = "B"; //esto no es valido
+
+//ejemplo de uso de caracter especial como nombre de variable
+
+let user@name = "Aylén"; //esto no es valido
+
+//agregar console.log para ver los valores
+
+console.log(nombre);
+console.log(_nombre);
+console.log($preciofinal);
+console.log(firstname);
