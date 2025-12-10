79fd931d52829d949526f3a989ff4c389060b794
Aylén Frísoli <aylen@trece.ar>
2025-12-10 20:05:03 -0300

    Clase 12

---
+++ clase-12/arrays.js
@@ -0,0 +1,18 @@
+//arrays
+let colores = ["Rosado", "Anaranjado", "Azul"];
+let frutas = ["Kiwi", "Frutilla","Sandia"];
+let numeros = [11,22,33,44];
+let valoresBooleanos = [true, false, true, false];
+let mezcladitos = [11, true, "Marian", colores];
+
+//mostrar un array completos
+
+console.log(frutas);
+
+//acceder un dato especifico dentro del array
+
+console.log(colores[0]); //rosado
+
+//arrays dentro del array
+
+console.log(mezcladitos[3][0]); //rosado
---
+++ clase-12/while-do-while.js
@@ -0,0 +1,19 @@
+//Vamos a crear un programa que imprima un contador progresivo
+//desde 5 horas hasta 1 utilizando tanto un bucle while como un bucle do-while
+//WHILE
+console.log ("Contador regresivo usando while: ");
+
+let contador = 5;
+while (contador >= 1 ) {
+    console.log(contador);
+    contador--;
+}
+console.log ("Contador regresivo usando dowhile: ");
+
+//DO WHILE
+let contador2 = 5;
+do {
+    console.log(contador2);
+    contador2--;
+} while (contador2 >= 1 );
+
